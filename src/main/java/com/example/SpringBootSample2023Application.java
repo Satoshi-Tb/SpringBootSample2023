c233203056b13120f67311e76bad0611457e5e23
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-16 01:10:14 +0900

    initial commit

---
+++ src/main/java/com/example/SpringBootSample2023Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSample2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSample2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootSample2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSample2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
