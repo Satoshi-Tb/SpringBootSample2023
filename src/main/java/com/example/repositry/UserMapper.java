28373d15aef65f5481f8d2ab11957d0eee4ce72b
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-22 17:46:54 +0900

    ユーザー一覧照会を実装

--- src/main/java/com/example/repositry/UserMapper.java
+++ src/main/java/com/example/repositry/UserMapper.java
@@ -1,5 +1,7 @@
 package com.example.repositry;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.user.model.MUser;
@@ -13,5 +15,8 @@ public interface UserMapper {
 	
 	/** ユーザー登録 */
 	public int insertOne(MUser user);
+	
+	/** ユーザー取得 */
+	public List<MUser> findMany();
 
 }
