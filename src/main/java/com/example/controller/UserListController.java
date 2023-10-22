7dac03e0f09adc4ff47ccc5e8b306ee08b3433c4
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-22 23:42:17 +0900

    動的SQLを利用した検索処理の実装

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -1,12 +1,16 @@
 package com.example.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
+import com.example.form.UserListForm;
 
 @Controller
 @RequestMapping("/user")
@@ -15,11 +19,17 @@ public class UserListController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	/** ユーザー一覧画面を表示 */
-	@GetMapping("/list")
-	public String getUserList(Model model) {
+	@RequestMapping(path = "/list", method = { RequestMethod.GET, RequestMethod.POST })
+	public String getUserList(Model model, @ModelAttribute UserListForm form) {
+		// formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
 		// ユーザー一覧取得
-		var userList = userService.getUsers();
+		var userList = userService.getUsers(user);
 		
 		// Modelに登録
 		model.addAttribute("userList", userList);
@@ -27,4 +37,5 @@ public class UserListController {
 		// ユーザー一覧を表示
 		return "user/list";
 	}
+	
 }
