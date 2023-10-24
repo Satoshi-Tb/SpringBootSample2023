4402dc1af8d6e15e02f8acfaae7df33f0e32251b
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-25 01:00:51 +0900

    インメモリユーザー認証実装

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig   {
 	
@@ -40,7 +45,26 @@ public class SecurityConfig   {
         
         return http.build();
 	}
-
+	
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		//パスワードの暗号化
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	/**
+	 * インメモリ認証
+	 * */
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService(){
+	    UserDetails user = User.builder()
+	        .username("user")
+	        .password(passwordEncoder().encode("password"))
+	        .authorities("ROLE_GENERAL")
+	        .build();
+	    return new InMemoryUserDetailsManager(user);
+	}
 	
 	
 }
