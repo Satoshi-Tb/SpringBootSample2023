9785ae6d52a3a7d552a6a1f8ec9237d94aac4f69
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-31 18:44:13 +0900

    テストデータ追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,6 +15,15 @@ INSERT INTO m_user (
 ) VALUES
 ('system@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'システム管理者', '2000-01-01', 21, 1, 1, 'ROLE_ADMIN')
 , ('user@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー1', '2000-01-01', 21, 2, 2, 'ROLE_GENERAL')
+, ('user2@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー2', '2000-01-01', 41, 2, 2, 'ROLE_GENERAL')
+, ('user3@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー3', '2000-01-01', 31, 1, 2, 'ROLE_GENERAL')
+, ('user4@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー4', '2000-01-01', 25, 1, 2, 'ROLE_GENERAL')
+, ('user5@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー5', '2000-01-01', 35, 2, 2, 'ROLE_GENERAL')
+, ('user6@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー6', '2000-01-01', 35, 2, 3, 'ROLE_GENERAL')
+, ('user7@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー7', '2000-01-01', 28, 2, 3, 'ROLE_GENERAL')
+, ('user8@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー8', '2000-01-01', 55, 1, 3, 'ROLE_GENERAL')
+, ('user9@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'ユーザー9', '2000-01-01', 50, 1, 3, 'ROLE_GENERAL')
+,('system2@co.jp', '$2a$10$rJyapIrvsHARwCNgporWLO6QIKXXezOpRrdb..7X0ea0VwZ5IldSy', 'システム管理者2', '2000-01-01', 31, 2, 1, 'ROLE_ADMIN')
 ;
 
 /* 部署マスタ */
@@ -24,6 +33,7 @@ INSERT INTO m_department (
 ) VALUES
 (1, 'システム管理部')
 , (2, '営業部')
+, (3, 'SI事業部')
 ;
 
 /* 給料テーブル */
