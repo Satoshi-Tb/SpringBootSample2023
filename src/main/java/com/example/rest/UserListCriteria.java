a56e746b4620f30db4c25891a6786494707e94c5
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-11-18 23:48:26 +0900

    パラメータに制約追加

--- src/main/java/com/example/rest/UserListCriteria.java
+++ src/main/java/com/example/rest/UserListCriteria.java
@@ -1,12 +1,19 @@
 package com.example.rest;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserListCriteria {
     private String userId;
     private String userName;
+    
+    
     private Integer offset;
+    
+    @NotNull
     private Integer page;
+    
+    @NotNull
     private Integer size;
 }
